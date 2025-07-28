881094203cc93b0a6d6ef4851289f42974e008a7
Alexis <alkaloge@cern.ch>
2025-07-28 04:50:10 -0500

    latest scripts

--- combine/copy_datacards_from_coffea_area.sh
+++ combine/copy_datacards_from_coffea_area.sh
@@ -1,5 +1,25 @@
+rm -fr UL2018
+rm -fr UL2017
+rm -fr UL2016
+rm -fr UL2016APV
+rm -fr ULRun2
 
-cp -r /uscms_data/d3/alkaloge/MetStudies/nAOD/Suu/new/SuuToTU_TToBLNu_SUNY/datacards/UL2018 .
-cp -r /uscms_data/d3/alkaloge/MetStudies/nAOD/Suu/new/SuuToTU_TToBLNu_SUNY/datacards/UL2017 .
-cp -r /uscms_data/d3/alkaloge/MetStudies/nAOD/Suu/new/SuuToTU_TToBLNu_SUNY/datacards/ULpostVFP_2016/ .
-cp -r /uscms_data/d3/alkaloge/MetStudies/nAOD/Suu/new/SuuToTU_TToBLNu_SUNY/datacards/ULpreVFP_2016 .
+cp -r /uscms_data/d3/alkaloge/MetStudies/nAOD/Suu/new/SuuToTU_TToBLNu_SUNY/datacards/hirak/UL2018 .
+cp -r /uscms_data/d3/alkaloge/MetStudies/nAOD/Suu/new/SuuToTU_TToBLNu_SUNY/datacards/hirak/UL2017 .
+
+echo copying UL2016
+mkdir UL2016
+cp -r /uscms_data/d3/alkaloge/MetStudies/nAOD/Suu/new/SuuToTU_TToBLNu_SUNY/datacards/hirak/ULpostVFP_2016/* UL2016/.
+
+echo copying UL2016APV
+mkdir UL2016APV
+cp -r /uscms_data/d3/alkaloge/MetStudies/nAOD/Suu/new/SuuToTU_TToBLNu_SUNY/datacards/hirak/ULpreVFP_2016/* UL2016APV/.
+
+mv UL2016/test_postVFP_2016_recoSuu_mass0_JetpTgt300.root UL2016/test_2016_recoSuu_mass0_JetpTgt300.root
+mv UL2016APV/test_preVFP_2016_recoSuu_mass0_JetpTgt300.root UL2016APV/test_2016APV_recoSuu_mass0_JetpTgt300.root
+
+
+mkdir ULRun2
+echo merging 2016 pre+post -- not really used anywhere --
+
+hadd -f ULRun2/test_2016APV_recoSuu_mass0_JetpTgt300.root UL2018/test*root UL2017/test*root UL2016/test*root UL2016APV/test*root
--- combine/step1.sh
+++ combine/step1.sh
@@ -1,16 +1,18 @@
 # create root files with proper dir
 
 var=${1}
-
+var="recoSuu_mass0"
 cut="300"
 #python3 SetupSuu.py --year 2017 --cut 500
 #python3 SetupSuu.py --year 2018 --cut 500
 python3 SetupSuu.py --year 2018 --cut ${cut} --var ${var}
 python3 SetupSuu.py --year 2017 --cut ${cut} --var ${var}
-python3 SetupSuu.py --year postVFP_2016 --cut ${cut} --var ${var}
-python3 SetupSuu.py --year preVFP_2016 --cut ${cut} --var ${var}
+python3 SetupSuu.py --year 2016 --cut ${cut} --var ${var}
+python3 SetupSuu.py --year 2016APV --cut ${cut} --var ${var}
 
 
-mkdir UL2016
-hadd  -f UL2016/step1_2016_${var}_JetpTgt${cut}.root ULpostVFP_2016/step1_postVFP_2016_${var}_JetpTgt${cut}.root ULpreVFP_2016/step1_preVFP_2016_${var}_JetpTgt${cut}.root
+hadd -f ULRun2/step1_Run2_recoSuu_mass0_JetpTgt300.root UL2018/step1*recoSuu_mass0_JetpTgt300.root UL2017/step1*recoSuu_mass0_JetpTgt300.root UL2016/step1*recoSuu_mass0_JetpTgt300.root  UL2016APV/step1*recoSuu_mass0_JetpTgt300.root
+
+#mkdir UL2016
+#hadd  -f UL2016/step1_2016_${var}_JetpTgt${cut}.root ULpostVFP_2016/step1_postVFP_2016_${var}_JetpTgt${cut}.root ULpreVFP_2016/step1_preVFP_2016_${var}_JetpTgt${cut}.root
 
